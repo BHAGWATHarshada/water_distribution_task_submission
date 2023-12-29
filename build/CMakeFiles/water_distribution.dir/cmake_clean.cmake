1ad09e52aa805cc919d416ce2d8e561356e810be
BHAGWATHarshada <124351821+BHAGWATHarshada@users.noreply.github.com>
2023-12-29 10:51:38 +0300

    Add files via upload

---
+++ build/CMakeFiles/water_distribution.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/water_distribution.dir/water_distribution.cpp.o"
+  "water_distribution"
+  "water_distribution.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/water_distribution.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
